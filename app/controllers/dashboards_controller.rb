b28bf522feb25155e67d6c1e61b9407e7fca951b
Daniel Merrill Silva <danielmerrillsilva@Daniels-MacBook-Pro.local>
2016-12-30 13:07:39 -0300

    Finish dashboard create

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,8 @@
 class DashboardsController < ApplicationController
   load_and_authorize_resource
+  def show
+    @dashboard = Dashboard.find(params[:id])
+  end
 
   def index
     @dashboards = current_user.dashboards
@@ -9,10 +12,22 @@ class DashboardsController < ApplicationController
 
   end
 
+  def create
+    new_params = dashboard_params.dup
+    @dashboard = Dashboard.new(new_params)
+    respond_to do |format|
+      if @dashboard.save
+        format.html{redirect_to action: :index}
+      else
+        format.html{redirect_to action: :new, notice: "Dashboard could not be created."}
+      end
+    end
+  end
+
   protected
   def dashboard_params
 		params.require(:dashboard).permit(
-      :name
+      :name, :user_id
 		)
 	end
 end
