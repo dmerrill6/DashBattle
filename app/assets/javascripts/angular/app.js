77f944006e2335f279f4d02c6d535162787d98d3
Sebastian Butorovic <sibutorovic@uc.cl>
2017-01-04 15:22:38 -0300

    color picker

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,7 +1,6 @@
 var dashBattle =
   angular.module('DashBattle', ['ngAnimate', 'dashBattleControllers',
-  'dashBattleDirectives', 'dashBattleFilters', 'gridster', 'chart.js'])
+  'dashBattleDirectives', 'dashBattleFilters', 'gridster', 'chart.js', 'color.picker'])
          .config(['$animateProvider', function($animateProvider){
            $animateProvider.classNameFilter(/^(?:(?!ng-animate-disabled).)*$/);
-
          }]);
