cb44dc67fe2fbd4a55c923f475b9c7a36266950c
Daniel Merrill Silva <danielmerrillsilva@Daniels-MacBook-Pro.local>
2017-01-04 15:33:10 -0300

    Push seeds

--- app/models/component.rb
+++ app/models/component.rb
@@ -1,4 +1,7 @@
 class Component < ApplicationRecord
 	has_many :component_dashboards
-  	has_many :dashboards, through: :component_dashboards
+	has_many :dashboards, through: :component_dashboards
+
+	validates_uniqueness_of :data_type
+	validates_presence_of :data_type
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Component.create({data_type: 'amount', width: '1', height: '1', name: 'Number Component' })
+Component.create({data_type: 'goal', width: '1', height: '1', name: 'Goal Component' })
+Component.create({data_type: 'line-chart', width: '2', height: '1', name: 'Line Component' })
+Component.create({data_type: 'bar-chart', width: '2', height: '1', name: 'Bar Component' })
+Component.create({data_type: 'pie-chart', width: '2', height: '1', name: 'Pie Component' })
